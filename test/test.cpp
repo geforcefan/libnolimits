a2d31814c2fc443c6098823a9a7e4ba22018d13f
Ercan Akyürek <ercan.akyuerek@gmail.com>
2017-05-23 23:33:47 +0200

    test changed

--- test/test.cpp
+++ test/test.cpp
@@ -2,5 +2,7 @@
 #include <libnolimits.h>
 
 int main() {
+    NoLimits::NoLimits2::Park *park = new NoLimits::NoLimits2::Park("/Users/ercanakyurek/Desktop/TestCoaster/parks/Hydra/Hydra.nl2park");
+    park->save("/Users/ercanakyurek/Desktop/TestCoaster/Hydra.clone.nl2park");
     return 0;
 }
