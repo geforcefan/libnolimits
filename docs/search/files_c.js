2b533f1c7ffe690ec472257458bac42b03942384
Ercan Akyürek <ercan.akyuerek@gmail.com>
2017-05-27 12:36:58 +0200

    docs updated

--- docs/search/files_c.js
+++ docs/search/files_c.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['railnode_2ecpp',['RailNode.cpp',['../_rail_node_8cpp.html',1,'']]],
-  ['railnode_2eh',['RailNode.h',['../_rail_node_8h.html',1,'']]],
+  ['railconnector_2ecpp',['RailConnector.cpp',['../_rail_connector_8cpp.html',1,'']]],
+  ['railconnector_2eh',['RailConnector.h',['../_rail_connector_8h.html',1,'']]],
+  ['railconnectorconnection_2ecpp',['RailConnectorConnection.cpp',['../_rail_connector_connection_8cpp.html',1,'']]],
+  ['railconnectorconnection_2eh',['RailConnectorConnection.h',['../_rail_connector_connection_8h.html',1,'']]],
   ['readme_2emd',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
   ['resourcefile_2ecpp',['ResourceFile.cpp',['../_resource_file_8cpp.html',1,'']]],
   ['resourcefile_2eh',['ResourceFile.h',['../_resource_file_8h.html',1,'']]],
